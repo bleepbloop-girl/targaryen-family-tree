7eb8d519302dc1674ab82c639decfe5deff2ff4e
bleepbloop-girl <remygtyndall@gmail.com>
2024-09-04 11:03:45 -0400

    Create source dir and subs, eleventy config file

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+    // Add specific configuration options here
+    
+    return {
+        dir: {
+            input: 'src', 
+            output: 'dist'
+        }
+    };
+};
